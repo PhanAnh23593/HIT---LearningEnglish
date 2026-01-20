b08d32f0272844c6ed8545e4d5e6c9b14e5bfc1f
Phan the anh <anh988998@gmail.com>
2026-01-21 02:33:40 +0700

    feat(dao): implement method to fetch all vocabularies from database

--- src/main/java/org/example/dao/VocabularyDAO.java
+++ src/main/java/org/example/dao/VocabularyDAO.java
@@ -1,9 +1,43 @@
 package org.example.dao;
 
 import org.example.model.Vocabulary;
+import org.example.utils.DatabaseConnection;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.ArrayList;
+import java.util.List;
 
 public class VocabularyDAO {
+    public List<Vocabulary> getAllVocabulary()
+    {
+        List<Vocabulary> vocabularyList = new ArrayList<>();
+        String sql = "select * from vocabularies";
+        try {
+            Connection con = DatabaseConnection.getConnection();
+            PreparedStatement ps = con.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                Vocabulary vocabulary = new Vocabulary();
+                vocabulary.setId(rs.getInt("id"));
+                vocabulary.setWord(rs.getString("word"));
+                vocabulary.setMeaning(rs.getString("meaning"));
+                vocabulary.setAudio(rs.getString("audio_url"));
+                vocabulary.setTag(rs.getString("tag"));
+                vocabulary.setIpa(rs.getString("ipa"));
+                vocabulary.setExample(rs.getString("example"));
+                vocabulary.setExampleMeaning(rs.getString("example_meaning"));
+                vocabularyList.add(vocabulary);
+            }
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return vocabularyList;
+    }
+
+
 
 }
